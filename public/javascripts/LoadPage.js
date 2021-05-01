a0b233de29542da4240d424e9d19d7f3b9de8683
jangyunhwan <jangyunhwan@gmail.com>
2021-05-02 01:19:07 +0900

    Update logic of top menu list
    get data from server for dynamic making dom on the top menus

--- public/javascripts/LoadPage.js
+++ public/javascripts/LoadPage.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
     $("#footer").load("./html/footer.html");
     $("#header").load("./html/header.html");
+
+    fetch('http://yhjang.shop:3000/menus')
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(info => {
+                $("#navigation").append(`<li><a href=".">${info.title}</a>&nbsp;&nbsp;</li>`);
+            });
+        })
+        .catch(err => {
+            console.log(' err : ' + err);
+        });
 });
