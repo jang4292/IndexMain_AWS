9a83c1fbcdd62006f9b5ce44b6b1e46c871e88f9
jangyunhwan <jangyunhwan@gmail.com>
2021-05-02 21:57:03 +0900

    Update main index html file to use semantic tag
    1. Change structure in main view
    2. Move unused or temporary files into unused folder

--- public/javascripts/LoadPage.js
+++ public/javascripts/LoadPage.js
@@ -1,15 +1,4 @@
 $(document).ready(function () {
     $("#footer").load("./html/footer.html");
     $("#header").load("./html/header.html");
-
-    fetch('http://yhjang.shop:3000/menus')
-        .then(res => res.json())
-        .then(data => {
-            data.forEach(info => {
-                $("#navigation").append(`<li><a href=".">${info.title}</a>&nbsp;&nbsp;</li>`);
-            });
-        })
-        .catch(err => {
-            console.log(' err : ' + err);
-        });
 });
