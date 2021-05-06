0a6d14f29759a2db5c778b6500826efab4338dbf
jangyunhwan <jangyunhwan@gmail.com>
2021-05-07 00:07:59 +0900

    Update navigation with social icons and etc

--- public/javascripts/Header.js
+++ public/javascripts/Header.js
@@ -2,7 +2,26 @@ const toggleBtn = document.querySelector('.navigation__toggle_button');
 const menu = document.querySelector('.navigation__menu');
 const icons = document.querySelector('.navigation__icons');
 
+const container = document.getElementById('container');
+
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     icons.classList.toggle('active');
+    container.classList.toggle('active');
 });
+
+fetch('http://yhjang.shop:3000/menus')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(info => {
+            const li = document.createElement('li');
+            const a = document.createElement('a');
+            a.href = '#';
+            a.innerText = `${info.title}`;
+            li.appendChild(a);
+            menu.appendChild(li);
+        });
+    })
+    .catch(err => {
+        console.log(' err : ' + err);
+    });
